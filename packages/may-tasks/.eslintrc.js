35ec8137d19af44914b8f6000499a434683b9f1c
Nikolai Kolosovskyi <mykola@oneplanetops.com>
2019-08-29 14:07:16 +0300

    changed linters

--- packages/may-tasks/.eslintrc.js
+++ packages/may-tasks/.eslintrc.js
@@ -1,19 +1,3 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"commonjs": true,
-		"es6": true,
-		"node": true
-	},
-	"extends": ["airbnb-base", "prettier"],
-	"plugins": ["prettier"],
-	"rules": {
-		"prettier/prettier": ["error"],
-		"indent": "off",
-		"import/no-extraneous-dependencies": [2, { devDependencies: true }],
-		"import/prefer-default-export": "off",
-		"no-plusplus": "off",
-		"camelcase": "off",
-		"no-use-before-define": ["error", { "functions": false }],
-	}
+	"extends": ["eslint-config-may-base"],
 }
