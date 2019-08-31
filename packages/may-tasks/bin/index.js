987af5a29ac292f63549b0afeca64f81b814c851
Nikolai Kolosovskyi <kpoccep@yandex.ru>
2019-08-31 16:09:28 +0300

    renamed package for development, fixed path for gulpfile

--- packages/may-tasks/bin/index.js
+++ packages/may-tasks/bin/index.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
 const spawn = require("cross-spawn");
+const path = require("path");
 
+const gulpfile = path.resolve(__dirname, '../', 'gulpfile.js');
 const cwd = process.cwd();
 
 const args = process.argv.slice(2);
-
-console.log("process.cwd()", process.cwd());
-console.log("process.argv", process.argv);
 
 spawn.sync(
 	"gulp",
 	[
 		"--gulpfile",
-		"../../node_modules/may-tasks/gulpfile.js",
+		gulpfile,
 		"--cwd",
 		cwd,
 		...args,
