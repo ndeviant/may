ea8ab4f6bb49d7c02f1dd0245ccc076f7600c89c
Nikolai Kolosovskyi <mykola@oneplanetops.com>
2019-09-10 21:11:32 +0300

    create may app

---
+++ packages/create-may-app/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split(".");
+const major = semver[0];
+
+if (major < 8) {
+	console.error(
+		`You are running Node ${currentNodeVersion}.\n` +
+			`Create React App requires Node 8 or higher. \n` +
+			`Please update your version of Node.`,
+	);
+	process.exit(1);
+}
+
+require("./createMayApp");
