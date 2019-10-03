d5a087ebc5950fabb3b33c889cd44376831cf66e
Nikolai Kolosovskyi <mykola@oneplanetops.com>
2019-10-03 14:54:51 +0300

    bumped version of may task in dev package

--- packages/may-tasks/root/webpack.config.js
+++ packages/may-tasks/root/webpack.config.js
@@ -35,7 +35,7 @@ const webpackConfig = {
 				exclude: /node_modules/,
 				use: [
 					{
-						loader: "babel-loader",
+						loader: require.resolve("babel-loader"),
 						options: {
 							presets: [
 								[require.resolve("@babel/preset-env"), { modules: false }],
