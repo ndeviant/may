ca7ee8f0e62dbf595222b5c7307acc6bec056c32
Nikolai Kolosovskyi <kpoccep@yandex.ru>
2019-11-10 15:55:19 +0200

    added isProduction to resolve function

--- packages/may-tasks/root/tasks/helpers/utils.js
+++ packages/may-tasks/root/tasks/helpers/utils.js
@@ -17,7 +17,7 @@ const resolveFunction = (userOption, defaultOption) => {
 	if (!userOption || typeof userOption !== "function") {
 		return userOption;
 	}
-	return userOption(defaultOption);
+	return userOption(defaultOption, isProduction);
 };
 
 module.exports.resolveFunction = resolveFunction;
